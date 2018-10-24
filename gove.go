955c5a0b089806aec9b8ce36be58c1e0450d94c2
Hideyuki Teramura <hide4d51@gmail.com>
2018-10-24 22:38:31 +0900

    Add main loop

---
+++ gove.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/veandco/go-sdl2/sdl"
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Gove - 0.1.0", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 640, 480, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	running := true
+	for running {
+		for evt := sdl.PollEvent(); evt != nil; evt = sdl.PollEvent() {
+			switch evt.(type) {
+			case *sdl.QuitEvent:
+				running = false
+				break
+			}
+		}
+	}
+}
